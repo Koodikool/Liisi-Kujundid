efae2e0e2bbcc0422cbe9fa2fecf70b68778653a
Liisi Kerik <liisikerik@hotmail.com>
2021-06-04 12:26:47 +0300

    Tee valitud kujundi nupp mitteklikitavaks.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function changeTab(shape) {
+function changeTab(button, shape) {
   for (const tabcontent of document.getElementsByClassName("tabcontent")) {
     tabcontent.hidden = true;
   }
+  button.disabled = true;
   document.getElementById(shape).hidden = false;
 }
