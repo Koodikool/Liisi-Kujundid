61743d60dc5b156f2206c430f48b081dcb9a5d0a
Liisi Kerik <liisikerik@hotmail.com>
2021-06-04 12:25:20 +0300

    Üleliigsete div-ide peitmise järel kuva õige div.

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function changeTab(shape) {
   for (const tabcontent of document.getElementsByClassName("tabcontent")) {
     tabcontent.hidden = true;
   }
+  document.getElementById(shape).hidden = false;
 }
