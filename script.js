99f4ee184b7d7bd47de922d623714e075bff1923
Liisi Kerik <liisikerik@hotmail.com>
2021-06-09 14:48:45 +0300

    Mis juhtub, kui külgede pikkused on 1, 2 ja 4? Kirjuta funktsioon, mis kontrollib külgede sobivust.

--- script.js
+++ script.js
@@ -8,11 +8,21 @@ function changeTab(button, shape) {
   button.disabled = true;
   document.getElementById(shape).hidden = false;
 }
+function err() {
+  document.getElementById("Perimeter").innerHTML = "VIGA";
+}
 function triangle() {
   const side1 = parseFloat(document.getElementById("TriangleSide1").value);
   const side2 = parseFloat(document.getElementById("TriangleSide2").value);
   const side3 = parseFloat(document.getElementById("TriangleSide3").value);
-  document.getElementById("Perimeter").innerHTML = trianglePerimeter(side1, side2, side3);
+  if (isTriangle(side1, side2, side3)) {
+    document.getElementById("Perimeter").innerHTML = trianglePerimeter(side1, side2, side3);
+  } else {
+    err();
+  }
+}
+function isTriangle(side1, side2, side3) {
+  return side1 < side2 + side3 && side2 < side1 + side3 && side3 < side1 + side2;
 }
 function trianglePerimeter(side1, side2, side3) {
   return side1 + side2 + side3;
