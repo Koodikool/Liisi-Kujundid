3c96cf5ed51ded043fb8ac745c02e1089cfc723f
Liisi Kerik <liisikerik@hotmail.com>
2021-06-09 14:46:13 +0300

    Paranda viga. Teisenda andmed tekstist komaga arvudeks.

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function changeTab(button, shape) {
   document.getElementById(shape).hidden = false;
 }
 function triangle() {
-  const side1 = document.getElementById("TriangleSide1").value;
-  const side2 = document.getElementById("TriangleSide2").value;
-  const side3 = document.getElementById("TriangleSide3").value;
+  const side1 = parseFloat(document.getElementById("TriangleSide1").value);
+  const side2 = parseFloat(document.getElementById("TriangleSide2").value);
+  const side3 = parseFloat(document.getElementById("TriangleSide3").value);
   document.getElementById("Perimeter").innerHTML = trianglePerimeter(side1, side2, side3);
 }
 function trianglePerimeter(side1, side2, side3) {
